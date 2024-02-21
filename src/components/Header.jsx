67ee0bc77a020f12b71c0f36345417f21fcd6bd9
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-02-21 22:28:16 +0500

    footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,45 @@
 
-import {NavLink, Link } from "react-router-dom";
+import {NavLink, Link, Outlet } from "react-router-dom";
 import logo from "../assets/logo.jpg";
-import { useEffect, useState } from "react";
 
 // Links
 import { Links } from "../utils/links";
+import { useEffect, useState } from "react";
 
-function Header() {
-     // theme
+function Header({language, onLanguage}) {
+  // theme
   const getTheme = localStorage.getItem("theme");
   const [mode, setMode] = useState(getTheme || "light");
   const element = document.documentElement;
 
   // mode
-  useEffect(() => {
-    switch (mode) {
-      case "dark":
-        element.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-        break;
-      case "light":
-        element.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-        break;
-      default:
-        break;
-    }
-  }, [mode]);
+ useEffect(() => {
+   switch (mode) {
+     case "dark":
+       element.classList.add("dark");
+       localStorage.setItem("theme", "dark");
+       break;
+     case "light":
+       element.classList.remove("dark");
+       localStorage.setItem("theme", "light");
+       break;
+     default:
+       break;
+   }
+ }, [mode]);
 
-  // language
-  if (!localStorage.getItem("language")) localStorage.setItem("language", "uz");
-  const [language, setlanguage] = useState(localStorage.getItem("language"));
+ const handleLanguageChange = (e) => {
+  localStorage.setItem("language", e.target.value);
+  onLanguage(e.target.value);
+};
 
-  const handleLanguageChange = (e) => {
-    localStorage.setItem("language", e.target.value);
-    setlanguage(e.target.value);
-  };
   return (
-    <header className="py-5">
+    <>
+      <header className="py-5">
         <div className="xl:container mx-auto px-10">
           <nav className="flex items-center justify-between">
             <Link to="/">
-              <img className="w-16 rounded-full" src={logo} alt="logo" />
+              <img className="w-16 rounded-full shadow-md" src={logo} alt="logo" />
             </Link>
             <div className="flex items-center gap-10">
               <div className="flex items-center gap-6">
@@ -80,6 +78,8 @@ function Header() {
           </nav>
         </div>
       </header>
+      <Outlet/>
+    </>
   )
 }
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Portfolio() {
   return <>
   <Header/>
         <div className="xl:container px-10 mx-auto">Portfolio</div>
+        <Footer/>
       </>;
 }
 
