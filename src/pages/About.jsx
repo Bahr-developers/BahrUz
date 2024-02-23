a09a47aa6c82cfa5694d96132ab366780fac0bb1
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-02-23 20:29:47 +0500

    conflig

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,20 +2,13 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function About() {
-<<<<<<< HEAD
   return (
     <>
       <Header />
       <div className="xl:container px-10 mx-auto">Biz haqimizda</div>
+      <Footer />
     </>
   );
-=======
-  return <>
-            <Header/>
-            <div className="xl:container px-10 mx-auto">Biz haqimizda</div>
-            <Footer/>
-        </>;
->>>>>>> origin/diyorbek
 }
 
 export default About;
