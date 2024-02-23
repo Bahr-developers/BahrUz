38a4d5db9ec14eb81228440abeae4501fbbc525f
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-02-23 20:47:37 +0500

    confilig

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,14 @@
-
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-<<<<<<< HEAD
-function Home() {
+function Home({ language, onLanguage }) {
   return (
     <>
-      <Header />
-      <div className="xl:container px-10 mx-auto">Home</div>
+      <Header language={language} onLanguage={onLanguage} />
+      <div className="xl:container px-10 mx-auto"></div>
+      <Footer language={language} />
     </>
   );
-=======
-function Home({language, onLanguage}) {
-  return <>
-        <Header language={language} onLanguage={onLanguage} />
-        <div className="xl:container px-10 mx-auto">
-          
-        </div>
-        <Footer language={language}/>
-          </>;
->>>>>>> origin/diyorbek
 }
 
 export default Home;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,20 +2,13 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Portfolio() {
-<<<<<<< HEAD
   return (
     <>
       <Header />
       <div className="xl:container px-10 mx-auto">Portfolio</div>
+      <Footer />
     </>
   );
-=======
-  return <>
-  <Header/>
-        <div className="xl:container px-10 mx-auto">Portfolio</div>
-        <Footer/>
-      </>;
->>>>>>> origin/diyorbek
 }
 
 export default Portfolio;
