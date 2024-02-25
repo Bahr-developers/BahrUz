79e82676663c92f136d24a93f2e7d796f3207546
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-02-26 00:17:54 +0500

    conflig

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
-
-import {NavLink, Link, Outlet } from "react-router-dom";
+import { NavLink, Link, Outlet } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 // Links
 import { Links } from "../utils/links";
 import { useEffect, useState } from "react";
 
 function Header() {
-  
   // language
   if (!localStorage.getItem("language")) localStorage.setItem("language", "uz");
   const [language, setLanguage] = useState(localStorage.getItem("language"));
@@ -18,33 +16,37 @@ function Header() {
   // Hamburger menu
   const [openMenu, setOpenMenu] = useState(false);
   // mode
- useEffect(() => {
-  switch (mode) {
-    case "dark":
-      element.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      break;
-    case "light":
-      element.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      break;
-    default:
-      break;
-  }
- }, [mode]);
+  useEffect(() => {
+    switch (mode) {
+      case "dark":
+        element.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        break;
+      case "light":
+        element.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        break;
+      default:
+        break;
+    }
+  }, [mode]);
 
   const handleLanguageChange = (e) => {
     localStorage.setItem("language", e.target.value);
     setLanguage(e.target.value);
   };
 
-return (
+  return (
     <>
       <header className="py-5">
         <div className="xl:container mx-auto px-10">
           <nav className="flex items-center justify-between">
             <Link to="/">
-              <img className="w-16 rounded-full shadow-md" src={logo} alt="logo" />
+              <img
+                className="w-16 rounded-full shadow-md"
+                src={logo}
+                alt="logo"
+              />
             </Link>
             <div className="flex items-center gap-10">
               <div className="flex items-center gap-6 ">
@@ -64,11 +66,15 @@ return (
                 className="pt-2 text-xl"
                 onClick={() => setMode(mode === "light" ? "dark" : "light")}
               >
-                    <ion-icon
-                    name={`${mode === "light" ? "sunny" : "moon"}`}
-                  ></ion-icon>
+                <ion-icon
+                  name={`${mode === "light" ? "sunny" : "moon"}`}
+                ></ion-icon>
               </button>
-                <select className="dark:bg-transparent" onChange={handleLanguageChange}>
+              <select
+                value={language}
+                className="dark:bg-transparent"
+                onChange={handleLanguageChange}
+              >
                 <option value="uz">uz</option>
                 <option value="ru">ru</option>
                 <option value="en">en</option>
@@ -79,52 +85,53 @@ return (
                 name="menu"
                 onClick={() => setOpenMenu(true)}
               ></ion-icon>
-            </button>          
-        </nav>
-        {/* menu */}
-        <div
-          className={`fixed top-0 left-0 w-[50%] bg-white min-h-screen  transition-w  ease-in-out  duration-500 md:hidden dark:bg-slate-700  dark:text-white ${
-            openMenu ? "w-[70%] h-[100vh]" : "w-0 opacity-0 pointer-events-none"
-          }`}
-        >
-          <button className="text-3xl flex items-center absolute top-2 right-2  bg-slate-400 dark:text-white rounded px-1 py-1 text-white ">
-            <ion-icon
-              name="close"
-              onClick={() => setOpenMenu(false)}
-            ></ion-icon>
-          </button>
-          <div>
-            <div className="py-1 px-2 rounded cursor-pointer dark:bg-slate-500 mt-16 max-w-[120px] mx-6 bg-slate-400 text-white">
-              <select
-                onChange={handleLanguageChange}
-                className="bg-transparent w-full outline-none cursor-pointer"
-                value={language}
-              >
-                <option value="uz">uz</option>
-                <option value="ru">ru</option>
-                <option value="en">en</option>
-              </select>
-            </div>
-            {Links.map((link) => {
-              return (
-                <Link
-                  to={link.to}
-                  key={link.id}
-                  className="text-black block dark:text-white px-6 py-4"
-                  onClick={() => setOpenMenu(false)}
+            </button>
+          </nav>
+          {/* menu */}
+          <div
+            className={`fixed top-0 left-0 w-[50%] bg-white min-h-screen  transition-w  ease-in-out  duration-500 md:hidden dark:bg-slate-700  dark:text-white ${
+              openMenu
+                ? "w-[70%] h-[100vh]"
+                : "w-0 opacity-0 pointer-events-none"
+            }`}
+          >
+            <button className="text-3xl flex items-center absolute top-2 right-2  bg-slate-400 dark:text-white rounded px-1 py-1 text-white ">
+              <ion-icon
+                name="close"
+                onClick={() => setOpenMenu(false)}
+              ></ion-icon>
+            </button>
+            <div>
+              <div className="py-1 px-2 rounded cursor-pointer dark:bg-slate-500 mt-16 max-w-[120px] mx-6 bg-slate-400 text-white">
+                <select
+                  onChange={handleLanguageChange}
+                  className="bg-transparent w-full outline-none cursor-pointer"
+                  value={language}
                 >
-                  {link.content[language]}
-                </Link>
-              );
-            })}
+                  <option value="uz">uz</option>
+                  <option value="ru">ru</option>
+                  <option value="en">en</option>
+                </select>
+              </div>
+              {Links.map((link) => {
+                return (
+                  <Link
+                    to={link.to}
+                    key={link.id}
+                    className="text-black block dark:text-white px-6 py-4"
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    {link.content[language]}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <Outlet />
+    </>
   );
-    <Outlet/>
-   </>
-  )
 }
 
 export default Header;
