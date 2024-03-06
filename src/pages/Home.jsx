829c16eb65bbdbad0899151fdf548a950af80338
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-03-06 09:52:29 +0500

    Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 import Partner from "../components/Partner";
 import Services from "../components/Services";
 
 function Home() {
-  return <>
-        <Header/>
-        <div className="xl:container mx-auto px-5 md:px-10 mx-auto">
-          <Partner/>
-          <Services/>
-        </div>
-        <Footer/>
-          </>
+  return (
+    <>
+      <Header />
+      <Hero />
+      <div className="xl:container mx-auto px-5 md:px-10">
+        <Partner />
+        <Services />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
