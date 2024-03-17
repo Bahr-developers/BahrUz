4a5d561f99e70dc01812fb27d43306e17a36ff46
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-03-17 13:28:47 +0500

    languages

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
   return (
-    <div className=" mx-auto mt-10">
+    <div className="container mx-auto mt-10">
       <div className="text-[100px] text-yellow-500 text-center">
         <ion-icon name="warning"></ion-icon>
       </div>
-      <p className="text-3xl text-center">Page not found</p>
+      <p className="text-3xl text-center mb-6">Page not found</p>
+      <div className="text-center">
+        <Link
+          to="/"
+          className="mx-auto border rounded bg-blue-400 text-white border-blue-400 hover:bg-white hover:text-blue-400 duration-150 py-2 px-2 text-center text-xl"
+        >
+          Go to Home page
+        </Link>
+      </div>
     </div>
   );
 }
