c8fc5c4f8c843c8faba3a1844baf6675bd9f4fa4
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-03-18 14:09:16 +0500

    commited

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,27 @@ function About({ language, setLanguage }) {
   return (
     <>
       <Header language={language} setLanguage={setLanguage} />
-      <div className="xl:container px-10 mx-auto">Biz haqimizda</div>
+      <div className="container mx-auto">
+        <div>
+          <h2 className="text-4xl my-8 text-sky-400 dark:text-slate-300">
+            Biz haqimizda
+          </h2>
+          <p className="text-lg mb-64">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            doloribus esse tenetur perferendis a excepturi labore, nisi iure qui
+            animi corrupti possimus quisquam? Cupiditate aspernatur optio
+            quidem. Accusamus nulla commodi molestias hic iure. Incidunt vel,
+            libero, porro vero ipsa dolorum alias quasi aliquam nulla
+            consequatur quae rerum minima inventore placeat voluptas, error iure
+            aut corporis quod repellat repellendus accusantium magnam velit
+            dignissimos! Ullam voluptates, molestias fugit earum tempore
+            officiis fugiat? Nostrum laborum quaerat corrupti tenetur hic
+            deleniti quo accusantium rem dolorum nisi consequatur libero numquam
+            saepe, molestiae cupiditate nulla, voluptatem facere quas magni
+            minus iste iure eligendi ex nemo! Ipsam.
+          </p>
+        </div>
+      </div>
       <Footer />
     </>
   );
---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,31 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ServicesCart from "../components/ServicesCart";
+import { servicesCartTitle } from "../utils/links";
+
+function ServicesPage({ language, setLanguage }) {
+  return (
+    <>
+      <Header language={language} setLanguage={setLanguage} />
+      <div className="container  mx-auto">
+        <div>
+          <h3 className="text-sky-400 dark:text-slate-300 text-4xl my-10">
+            Services
+          </h3>
+          <div className="card-wrap mt-5 grid md:grid-cols-2 gap-3 mb-10">
+            {servicesCartTitle[language].services?.map((service) => (
+              <ServicesCart
+                key={service.id}
+                title={service.title}
+                subtitle={service.subtitle}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default ServicesPage;
